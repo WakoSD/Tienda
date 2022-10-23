f83031bd6c3b86f369fac0879f216e32e8b3c9b0
Jerry Campos <jerrycampos02@gmail.com>
2022-10-23 13:40:42 -0600

    Semana 4 -  IndexController, dependencies for SQL, ClienteDAO

---
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -0,0 +1,34 @@
+
+package com.tienda.controller;
+
+import com.tienda.dao.ClienteDao;
+import com.tienda.domain.Cliente;
+import java.util.Arrays;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class IndexController {
+    
+    @Autowired
+    private ClienteDao clienteDao;
+    
+    @GetMapping("/")
+    public String inicio(Model model){
+        var texto = "Estamos en semana 4"; 
+        model.addAttribute("mensaje", texto); 
+        
+        var clientes=clienteDao.findAll();
+        
+        model.addAttribute("clientes",clientes);
+        
+        return "index";
+    }
+    
+    
+}
+ 
